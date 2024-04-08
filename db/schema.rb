701c6bae2637043e95e8966f4632a763b787f686
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-04-08 20:47:39 +0900

    p151のUserモデルを作るのサンプルコード
    bin/rails g model user name:string email:string password_digest:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_19_114922) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_08_114410) do
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
