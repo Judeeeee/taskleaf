694600b74269554c764dff798366a1939cb4113d
siso25 <siso25@zoho.com>
2024-04-29 21:04:24 +0900

    「P.176 絞り込み条件」 の前まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_15_111658) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_29_110551) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
