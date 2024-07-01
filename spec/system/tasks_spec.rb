774cb82558873a128e9952aae421f0385c074d08
siso25 <siso25@zoho.com>
2024-07-01 21:34:04 +0900

    p224まで

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-    let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
-    before do
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
 
   shared_examples_for 'ユーザーAが作成したタスクが表示される' do
     it { expect(page).to have_content '最初のタスク' }
@@ -46,6 +46,7 @@ describe 'タスク管理機能', type: :system do
 
   describe '新規作成機能' do
     let(:login_user) { user_a }
+    let(:task_name) { '新規作成のテストを書く' } # デフォルトとして設定
 
     before do
       visit new_task_path
@@ -54,8 +55,6 @@ describe 'タスク管理機能', type: :system do
     end
 
     context '新規作成画面で名称を入力したとき' do
-      let(:task_name) { '新規作成のテストを書く' }
-
       it '正常に登録される' do
         expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
       end
